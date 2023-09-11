f66b9dfd3f8c3b69532361ad2da1f63eab543765
me <tankkiller0404@gmail.com>
2023-09-11 16:07:51 +0200

    ships.py, entities.py file creation main.py: pygame initialization and simple game loop icon and background sprite added

---
+++ entities.py
@@ -0,0 +1,5 @@
+class Player:
+    ship = ""
+
+    #def __init_(self, Ship_type):
+    #   self.ship = Ship_type
--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print("het is me gelukt om iets op Github te zetten! Jeeej")
+import pygame
+
+# initialize pygame
+pygame.init()
+pygame.font.init()
+
+# create the screen
+height = 1000
+width = 1000
+screen = pygame.display.set_mode((width, height))
+
+# title and icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load("sprites/icon.png")
+pygame.display.set_icon(icon)
+
+# set sprites
+# background sprite
+background = pygame.image.load("sprites/background.jpg")
+background = pygame.transform.scale(background, (width, height/3))
+
+# game loop
+running = True
+while running:
+    screen.blit(background, (width, 0))
+    screen.blit(background, (width, height/3))
+    screen.blit(background, (width, 2*height/3))
+
+    for event in pygame.event.get():
+        # Quit/anti-freeze
+        if event.type == pygame.QUIT:
+            running = False
